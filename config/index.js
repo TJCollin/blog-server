66970fd4532f05122e3965247b36ba35ba298ebf
collin <tjzhangcong@outlook.com>
2021-09-28 19:39:45 +0800

    修改mongo地址

--- config/index.js
+++ config/index.js
@@ -1,36 +1,34 @@
-'use strict'
+"use strict";
 
 const REDIS = {
-	port: '6379',
-	server: '127.0.0.1'
-}
+  port: "6379",
+  server: "127.0.0.1",
+};
 
 const JWT = {
-	secret: 'collin',
-	expiresIn: 60*60*24// 授权时效24小时
-}
+  secret: "collin",
+  expiresIn: 60 * 60 * 24, // 授权时效24小时
+};
 
 const USER = {
-	defaultUsername: 'collin',
-	defaultPassword: 'collin',
-    jwtTokenSecret: 'collin'
-}
+  defaultUsername: "collin",
+  defaultPassword: "collin",
+  jwtTokenSecret: "collin",
+};
 
 const APP = {
-	port: '3000',
-	root_path: 'api'
-}
+  port: "3000",
+  root_path: "api",
+};
 
 const Mongo = {
-	uri: 'mongodb://127.0.0.1:27017/my_blog',
-}
-
-
+  uri: "mongodb+srv://collin:collin@cluster0.nbzgs.mongodb.net/my_blog",
+};
 
 export default {
-	REDIS,
-	JWT,
-	USER,
-	Mongo,
-	APP
-}
+  REDIS,
+  JWT,
+  USER,
+  Mongo,
+  APP,
+};
